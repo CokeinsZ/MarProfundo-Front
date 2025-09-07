2a2b71b671ecbd9a8b0f4d56598c7367a5cee775
Juanf <juanfe.yepes05@gmail.com>
2025-09-07 17:27:23 -0500

    folders webpage, components, interfaces,schemas created

---
+++ app/(webpage)/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@/components/organisms/header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+
+export const metadata: Metadata = {
+  title: "Mar abierto",
+  description: "proyecto front end para la universidad autónoma de manizales",
+};
+
+export default function webpageLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
+      >
+        {/* Header */}
+        <Header />
+
+        {/* Main Content */}
+        <main className="mx-auto max-w-6xl p-6">{children}</main>
+      </body>
+    </html>
+  );
+}
